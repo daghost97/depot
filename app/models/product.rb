2aaa994d41eeb9c6d404925dfbeadf12fd508526
Shaun Parker <dparker97@gmail.com>
2013-03-06 23:06:56 -0500

    Updated product validation, along with the functional and unit test

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,15 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+  
+  # The field's title, description, and image URL are not empty.
+  validates :title, :description, :image_url, presence: true
+  # The price is a valid number not less than $0.01.
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  # The title is unique among all products.
+  validates :title, uniqueness: true
+  # The image URL has gif, jpg or png extension.
+  validates :image_url, allow_blank: true, format: {
+    with:     %r{\.(gif|jpg|png)\Z}i,
+    message:  'must be a URL for GIF, JPG or PNG image.'
+  }
 end
